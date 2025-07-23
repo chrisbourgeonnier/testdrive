c2499e448d437841df3f287d48a0f5551f6bb663
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2025-07-23 11:32:47 +0930

    make changes to templates and admin vehicles edit

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -5,6 +5,7 @@ from .models import Vehicle
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ['id', 'make', 'model', 'year', 'price', 'is_active']
+    list_display_links = ['id', 'make', 'model']
     search_fields = ['make', 'model', 'year']
     list_filter = ['make', 'year', 'is_active']
     ordering = ['make', 'model', 'year']
