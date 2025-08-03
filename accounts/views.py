fae057102333ee77d8bc15135e831ebceb4297c6
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2025-08-03 18:00:36 +0930

    Create Log In form for User and Admin, with logic for Admin only

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class LoginPageView(TemplateView):
+    template_name = 'login/login.html'
