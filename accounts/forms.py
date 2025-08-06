54e9de60dfd28c5d25b8126682492bbb1f0e434f
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2025-08-06 14:08:00 +0930

    Add new fields to registration form (names, phone, dob)

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,47 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from captcha.fields import CaptchaField
 from .models import UserProfile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True, label='First Name')
+    last_name = forms.CharField(max_length=30, required=True, label='Last Name')
+    phone_number = forms.CharField(max_length=20, required=True, label='Phone Number')
+    dob = forms.DateField(
+        required=True,
+        label='Date of Birth',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+    )
+    captcha = CaptchaField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+            "phone_number",
+            "dob",
+            "captcha",
+        ]
+
+    def save(self, commit=True):
+        user = super().save(commit)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+            profile, _ = UserProfile.objects.get_or_create(user=user)
+            profile.phone_number = self.cleaned_data["phone_number"]
+            profile.dob = self.cleaned_data["dob"]
+            profile.save()
+        return user
 
 class UserWithProfileForm(forms.ModelForm):
     phone_number = forms.CharField(max_length=20, required=False, label='Phone Number')
@@ -16,8 +49,19 @@ class UserWithProfileForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email',
-                  'phone_number', 'dob', 'is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions']
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "dob",
+            "is_staff",
+            "is_active",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     phone_number = models.CharField(max_length=20, blank=True)
-    dob = models.DateField(default=timezone.now)  # Same pattern as bookings.Booking
+    dob = models.DateField(default=timezone.now)
 
     def __str__(self):
         return f"Profile for {self.user.username}"
