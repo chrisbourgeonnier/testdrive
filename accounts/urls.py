41b68d4f727b5422fd67ad30a498e4738afc935a
Mayerli Almario <mayerlialmario@gmail.com>
2025-08-11 22:15:41 +0930

    feat(accounts): add user dashboard view and template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginPageView, RegisterPageView
+from .views import LoginPageView, RegisterPageView, UserDashboardView
 
 urlpatterns = [
     path('login/', LoginPageView.as_view(), name='login'),
     path('register/', RegisterPageView.as_view(), name='register'),  # ✅ New
+    path('dashboard/', UserDashboardView.as_view(), name='user_dashboard'),  # New user_dashboard
 ]
